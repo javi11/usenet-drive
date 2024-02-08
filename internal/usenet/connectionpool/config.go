74ea563ebc3d39e56b84675969a4fa8a852df3a1
javi11 <javicuba11@gmail.com>
2024-02-08 22:46:50 +0100

    perf: better managment of the conn pool BREAKING CHANGE: move max_connection_ttl_in_minutes outside download config
    fix: make buffered channels

--- internal/usenet/connectionpool/config.go
+++ internal/usenet/connectionpool/config.go
@@ -9,20 +9,24 @@ import (
 )
 
 type Config struct {
-	downloadProviders []config.UsenetProvider
-	uploadProviders   []config.UsenetProvider
-	log               *slog.Logger
-	fakeConnections   bool
-	cli               nntpcli.Client
-	maxConnectionTTL  time.Duration
+	downloadProviders      []config.UsenetProvider
+	uploadProviders        []config.UsenetProvider
+	log                    *slog.Logger
+	fakeConnections        bool
+	cli                    nntpcli.Client
+	maxConnectionTTL       time.Duration
+	maxConnectionIdleTime  time.Duration
+	minDownloadConnections int
 }
 
 type Option func(*Config)
 
 func defaultConfig() *Config {
 	return &Config{
-		fakeConnections:  false,
-		maxConnectionTTL: 10 * time.Minute,
+		fakeConnections:        false,
+		maxConnectionTTL:       60 * time.Minute,
+		maxConnectionIdleTime:  30 * time.Minute,
+		minDownloadConnections: 5,
 	}
 }
 
@@ -59,5 +63,17 @@ func WithFakeConnections(fakeConnections bool) Option {
 func WithMaxConnectionTTL(maxConnectionTTL time.Duration) Option {
 	return func(c *Config) {
 		c.maxConnectionTTL = maxConnectionTTL
+	}
+}
+
+func WithMaxConnectionIdleTime(maxConnectionIdleTime time.Duration) Option {
+	return func(c *Config) {
+		c.maxConnectionIdleTime = maxConnectionIdleTime
+	}
+}
+
+func WithMinDownloadConnections(minDownloadConnections int) Option {
+	return func(c *Config) {
+		c.minDownloadConnections = minDownloadConnections
 	}
 }
